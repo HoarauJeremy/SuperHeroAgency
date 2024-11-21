89c6825ac729a398f4dd951d7e72aadfffe8dae8
Jeremy Hoarau <jeremyhoarau44@gmail.com>
2024-11-21 15:01:59 +0400

    Ajout du CRUD pour Super Pouvoir

---
+++ src/Controller/SuperPouvoirController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\SuperPouvoir;
+use App\Form\SuperPouvoirType;
+use App\Repository\SuperPouvoirRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/super/pouvoir')]
+final class SuperPouvoirController extends AbstractController
+{
+    #[Route(name: 'app_super_pouvoir_index', methods: ['GET'])]
+    public function index(SuperPouvoirRepository $superPouvoirRepository): Response
+    {
+        return $this->render('super_pouvoir/index.html.twig', [
+            'super_pouvoirs' => $superPouvoirRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_super_pouvoir_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $superPouvoir = new SuperPouvoir();
+        $form = $this->createForm(SuperPouvoirType::class, $superPouvoir);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($superPouvoir);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_super_pouvoir_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('super_pouvoir/new.html.twig', [
+            'super_pouvoir' => $superPouvoir,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_super_pouvoir_show', methods: ['GET'])]
+    public function show(SuperPouvoir $superPouvoir): Response
+    {
+        return $this->render('super_pouvoir/show.html.twig', [
+            'super_pouvoir' => $superPouvoir,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_super_pouvoir_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, SuperPouvoir $superPouvoir, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(SuperPouvoirType::class, $superPouvoir);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_super_pouvoir_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('super_pouvoir/edit.html.twig', [
+            'super_pouvoir' => $superPouvoir,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_super_pouvoir_delete', methods: ['POST'])]
+    public function delete(Request $request, SuperPouvoir $superPouvoir, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$superPouvoir->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($superPouvoir);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_super_pouvoir_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
