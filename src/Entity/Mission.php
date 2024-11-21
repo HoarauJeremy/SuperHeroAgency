20a894de3b944a7f2cedc42889f570d7a6fd6418
Jeremy Hoarau <jeremyhoarau44@gmail.com>
2024-11-21 14:55:27 +0400

    Création de l'entité Mission

---
+++ src/Entity/Mission.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Statut;
+use App\Repository\MissionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MissionRepository::class)]
+class Mission
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: 'string', enumType: Statut::class, nullable: false)]
+    private ?Statut $statut = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $debut = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $fin = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $localisation = null;
+
+    #[ORM\ManyToOne(inversedBy: 'missions')]
+    private ?Equipe $equipeEnCharge = null;
+
+    #[ORM\Column]
+    private ?int $niveauDanger = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStatut(): Statut
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(Statut $statut): self
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    public function getDebut(): ?\DateTimeInterface
+    {
+        return $this->debut;
+    }
+
+    public function setDebut(\DateTimeInterface $debut): static
+    {
+        $this->debut = $debut;
+
+        return $this;
+    }
+
+    public function getFin(): ?\DateTimeInterface
+    {
+        return $this->fin;
+    }
+
+    public function setFin(\DateTimeInterface $fin): static
+    {
+        $this->fin = $fin;
+
+        return $this;
+    }
+
+    public function getLocalisation(): ?string
+    {
+        return $this->localisation;
+    }
+
+    public function setLocalisation(string $localisation): static
+    {
+        $this->localisation = $localisation;
+
+        return $this;
+    }
+
+    public function getEquipeEnCharge(): ?Equipe
+    {
+        return $this->equipeEnCharge;
+    }
+
+    public function setEquipeEnCharge(?Equipe $equipeEnCharge): static
+    {
+        $this->equipeEnCharge = $equipeEnCharge;
+
+        return $this;
+    }
+
+    public function getNiveauDanger(): ?int
+    {
+        return $this->niveauDanger;
+    }
+
+    public function setNiveauDanger(int $niveauDanger): static
+    {
+        $this->niveauDanger = $niveauDanger;
+
+        return $this;
+    }
+}
